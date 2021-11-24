15229d26fd2eb61bb4bde00c3a1cd52a58ae3e69
agnesrausz <rauszka94@gmail.com>
2021-11-24 21:19:25 +0100

    Create view migration

---
+++ app/Database/Migrations/2021-11-24-025153_add_view.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddView extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'image_id' => [
+                'type' => 'INT',
+                'constraint' => 11
+            ],
+            'view_count' => [
+                'type' => 'INT',
+                'constraint' => 11
+            ]
+        ]);
+        $this->forge->addForeignKey('image_id', 'images', 'id');
+        $this->forge->createTable('views');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('views');
+    }
+}
