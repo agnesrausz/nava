ff3fb3735c6465b843ef6644d109fbc8a16cd21d
agnesrausz <rauszka94@gmail.com>
2021-12-03 04:19:46 +0100

    Implement App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link,
+    useParams,
+    useRouteMatch
+} from 'react-router-dom';
+import Home from "./components/Home";
+import Image from "./components/Image";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route exact path='/' element={<Home/>} />
+                <Route exact path='/images' element={<Home/>} />
+                <Route exact path='/image/:id' element={<Image/>} />
+            </Routes>
+        </BrowserRouter>
+
+
+    );
 }
 
 export default App;
