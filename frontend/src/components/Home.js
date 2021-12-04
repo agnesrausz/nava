7e874d2e1a06d3b198b0af3a2aba337cccf4803d
agnesrausz <rauszka94@gmail.com>
2021-12-04 20:40:11 +0100

    Adding setImage prop to Form and Table

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,14 +4,16 @@ import Table from "./Table";
 
 function Home(props) {
     const [isChange, setIsChange] = useState(false);
-    const [image, setImage] = useState({});
+    const [image, setImage] = useState();
 
 
 
     return (
         <div className='home'>
-            <Form  setIsChange={setIsChange} image={image}/>
-            <Table setIsChange={setIsChange} isChange={isChange}/>
+            {/*<Form  setIsChange={setIsChange} image={image}/>*/}
+            {/*<Table setIsChange={setIsChange} isChange={isChange}/>*/}
+            <Form  setIsChange={setIsChange} image={image} setImage={setImage}/>
+            <Table setIsChange={setIsChange} isChange={isChange} setImage={setImage}/>
         </div>
     );
 }
