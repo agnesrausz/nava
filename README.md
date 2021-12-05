92284fc19516cd0f3c560c98b43eac723c412905
agnesrausz <rauszka94@gmail.com>
2021-12-05 17:16:45 +0100

    Create dump.sql

---
+++ README.md
@@ -0,0 +1,98 @@
+# Nava
+## Main Stacks
+
+[![N|Solid](http://www.ahmetd.com/works/ci/ci-new-logo-04-02.jpg)](https://codeigniter.com/user_guide/intro/index.html)
+[![N|Solid](https://camo.githubusercontent.com/a492d6882b7e8703c681f48e72176860187c8fe9158f23f7a3707cb48bda4a32/68747470733a2f2f626c6f672d6173736574732e726973696e67737461636b2e636f6d2f323031362f4a616e2f72656163745f626573745f7072616374696365732d313435333231313134363734382e706e67)](https://reactjs.org/)
+
+
+Nava is web application with RESTFUL API created for NAVA interview homework.
+
+
+# Features
+Nava handles CRUD operation
+- Create images
+- Read image
+- Update already created images
+- Delete images
+
+# Tech
+
+NAVA project uses a number of open source projects:
+
+- [Codeigniter]
+- [React]
+- [Composer]
+- [MariaDB]
+- [MYSQL]
+
+
+# Installation
+
+### Backend
+Requirements:
+ - PHP version 7.3 or newer is required, with the [\*intl\* extension] and [\*mbstring\* extension] installed.
+ - The following PHP extensions should be enabled on your server: *php-json, php-mysqlnd, php-xml*
+ - MySQL (5.1+) via the MySQLi driver
+
+ - Composer package manager installed. (minimum version 2.1.6)
+
+Install the MYSQL dump
+```sh
+mysql -u [username] -p
+CREATE DATABASE database_name;
+USE database_name;
+SOURCE PATH/TO/dump.sql
+```
+
+Install the dependencies and start the server.
+```sh
+cd nava/backend
+composer install
+```
+Create the .env file based on the env file in the nava/backend directory.
+
+Set the database configuration in the .env file under the # DATABASE comment.
+And after that we can start the server:
+```sh
+php spark serve
+```
+
+### Frontend
+
+Newest Node version is required.
+
+```sh
+cd nava/frontend
+npm install
+```
+Copy the envExample.js to nava/frontend and rename it to: env.js
+And after that we can start the server:
+```sh
+npm start
+```
+# RESTful Routes
+
+Nava contains the following routes to provide response:
+
+| URL | HTTP Verb |  Action |  Description
+| ------ | ------ | ------ |------ |
+| /images | GET  | Index | Show all images
+| /images | POST  | Create | Create a new image
+| /images/:id | GET | Show | Show image with :id
+| /images/:id | PUT  | Update |  Update image with :id
+| /images/:id | DELETE  | Destroy |  Delete image with :id
+
+## Challenges
+
+To write both the back-end and front-end, switching the mindset was rather challenging and exhausting.
+It was scary at first to think about I have to learn a new framework and understand the concepts in days but since I know Laravel it was not that hard to make it work. 
+
+
+[//]: # (These are reference links used in the body of this note and get stripped out when the markdown processor does its job. There is no need to format nicely because it shouldn't be seen. Thanks SO - http://stackoverflow.com/questions/4823468/store-comments-in-markdown-syntax)
+
+   [MYSQL]: <https://www.mysql.com/>
+   [React]: <https://reactjs.org/>
+   [Codeigniter]: <https://codeigniter.com/>
+   [Composer]: <https://getcomposer.org/>
+   [\*intl\* extension]: <https://www.php.net/manual/en/intl.requirements.php>
+   [\*mbstring\* extension]: <https://www.php.net/manual/en/mbstring.requirements.php>
---
+++ dump.sql
@@ -0,0 +1,106 @@
+-- MySQL dump 10.19  Distrib 10.3.31-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: 127.0.0.1    Database: nava
+-- ------------------------------------------------------
+-- Server version	10.3.31-MariaDB-0ubuntu0.20.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `creator` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `images`
+--
+
+LOCK TABLES `images` WRITE;
+/*!40000 ALTER TABLE `images` DISABLE KEYS */;
+INSERT INTO `images` VALUES (1,'Járműipar - Ikarus autóbusz','Gere László'),(2,'Járműipar Háborús bűnösök - Szálasi Ferenc','Reismann Mariann'),(3,'Légi közlekedés - Húsz éves a Ferihegyi repülőtér','Bara István'),(4,'Vállalat - Autóbuszmodellek készülnek a Fővárosi\nAutóbuszüzemben','Pálfai Gábor'),(5,'Közlekedés - Budapest - A MÁV modern személyvonata','Faludi Imre');
+/*!40000 ALTER TABLE `images` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `migrations`
+--
+
+DROP TABLE IF EXISTS `migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `migrations` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `version` varchar(255) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `group` varchar(255) NOT NULL,
+  `namespace` varchar(255) NOT NULL,
+  `time` int(11) NOT NULL,
+  `batch` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `migrations`
+--
+
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` VALUES (7,'2021-11-24-024909','App\\Database\\Migrations\\AddImage','default','App',1638716248,1),(8,'2021-11-24-025153','App\\Database\\Migrations\\AddView','default','App',1638716248,1);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `views`
+--
+
+DROP TABLE IF EXISTS `views`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `views` (
+  `image_id` int(11) NOT NULL,
+  `view_count` int(11) NOT NULL,
+  UNIQUE KEY `image_id` (`image_id`),
+  CONSTRAINT `views_image_id_foreign` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `views`
+--
+
+LOCK TABLES `views` WRITE;
+/*!40000 ALTER TABLE `views` DISABLE KEYS */;
+INSERT INTO `views` VALUES (1,0),(2,8),(3,2),(4,9),(5,21);
+/*!40000 ALTER TABLE `views` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-05 16:04:37
